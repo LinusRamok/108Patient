c7aa9264f9d240732ab0560fcc7a84d0b718cdd4
kamlesh sahu <vasubantikamlesh@gmail.com>
2018-02-12 22:29:51 +0530

    bhai bhai

---
+++ app/build/generated/source/buildConfig/debug/com/Emergency/Patient/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Emergency.Patient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Emergency.Patient";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10101;
+  public static final String VERSION_NAME = "1.1.1";
+}
